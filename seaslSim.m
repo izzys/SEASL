cc133db3c6a3176e29d9039a862e5bdaff87f422
izzys <izzy.schall@gmail.com>
2015-08-19 13:09:05 +0300

    LC to compare

--- seaslSim.m
+++ seaslSim.m
@@ -14,17 +14,13 @@ Sim.Mod = Sim.Mod.Set('Phase','stance','LinearMotor','out');
 % Init controller:                                                                                short          extend
 Sim.Con = Sim.Con.Set('Period',1.2,'phi_tau',[0.1 0.25 0.4 0.99],'tau',[2 -3],'phi_reflex',[ 0.897308852443590    0.5  ]); 
 Sim.Con.Controller_Type = 'CPG';
- Sim.Con.IC = 0.710126001724011; %all reflex LC
-% Sim.Con.IC = 0.711880736600654; %only extend reflex LC (short phase 0.897308852443590)
-% Sim.Con.IC = 0.718629453132761; %only short reflex LC (extend phase 0.5)
-%Sim.Con.IC =   0.711880736605211;%no reflex LC (short phase 0.897308852443590 ,extend phase 0.5)
+Sim.Con.IC = 0.710126001724011; % LC
+
 Sim.Con.Init();
 
 % note that if IC match the stance phase - only the first two IC count:
- Sim.Mod.IC =   [ 0.359391210427136  -3.525464278857524  NaN NaN]; %all reflex LC
-% Sim.Mod.IC =  [  0.359391210428062 -3.537893670232675 NaN NaN]; % only extend reflex LC
-% Sim.Mod.IC =  [  0.359391210428062 -3.578743845708979 NaN NaN]; %only short reflex LC
-%Sim.Mod.IC =   [  0.359391210428063 -3.537893670364481 NaN NaN]; %no reflex LC
+ Sim.Mod.IC =   [ 0.359391210427136  -3.525464278857524  NaN NaN]; % LC
+
 Sim = Sim.Init();
 
 % Simulate:
