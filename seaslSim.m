7b2d7429023fd011d1a22023ef36b0f94297f987
izzys <izzy.schall@gmail.com>
2015-08-23 19:34:51 +0300

    Revert "ok2?"
    This reverts commit dc8f92d222788423b156fb764fe54a5ac579d7df.

--- seaslSim.m
+++ seaslSim.m
@@ -9,14 +9,19 @@ Sim.EndCond = 2;
 
 Sim = Sim.SetTime(0,0.02,100);
 
+
 % Set up the model:
 Sim.Mod = Sim.Mod.Set('Phase','stance','LinearMotor','out');
 
-% Init controller:                                                                          short  extend
-Sim.Con = Sim.Con.Set('Period',1.2,'phi_tau',[0.1 0.25 0.4 0.99],'tau',[2 -3],'phi_reflex',[ 0.9   NaN  ]); 
+
+
+% Init controller:                                                                    short             extend
+Sim.Con = Sim.Con.Set('Period',1.2,'phi_tau',[0.1 0.25 0.4 0.99],'tau',[2 -3],...
+                       'phi_reflex',[ 0.897308852443590  0.558365283467955 ]); %0.897308852443590   0.558365283467955
+% Sim.Con = Sim.Con.Set('Period',1.2,'phi_tau',[0.1 0.25 0.4 0.99],'tau',[2 -3],...
+%                        'phi_reflex',[ NaN  NaN ]); %0.897308852443590   0.558365283467955
 Sim.Con.Controller_Type = 'CPG';
-Sim.Con.IC =  0.718629453138436; %all reflex LC
-Sim.Con.IC =  0.711880736600654; %extend reflex LC
+
 
 
 Sim.IClimCyc = [ 0.359391210427835  -3.525464278622958  0   2.970203654740103   0.710126001714929] ;
@@ -30,6 +35,7 @@ Sim.Con.Init();
 
 Sim.Mod.IC =  [  0.359391210428054 -3.578743845550021 NaN NaN]; %all reflex LC
 Sim.Mod.IC =  [  0.359391210428062 -3.537893670232675 NaN NaN]; %extend reflex LC
+
 
 Sim = Sim.Init();
 
