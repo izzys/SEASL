b3075bfa5aff648152e69564102d6b0a952a91d3
izzys <izzy.schall@gmail.com>
2015-08-23 19:21:47 +0300

    commit before change to ROA

--- seaslSim.m
+++ seaslSim.m
@@ -6,13 +6,13 @@ Sim.Graphics = 1;
 %1: number of steps, 2: covergance
 
 Sim.EndCond = 2;
-Sim = Sim.SetTime(0,0.02,100);
+Sim = Sim.SetTime(0,0.01,100);
 
 % set terrain:
-Sim.Env.DisturbanceMaxHeight = 0.05;
+Sim.Env.DisturbanceMaxHeight = 0.03;
 Sim.Env.DisturbanceSign = 1;
-Sim.Env.start_x = 10;
-Sim.Env.end_x = 15;
+Sim.Env.start_x = 2;
+Sim.Env.end_x = 4;
 
 % Set up the model:
 
@@ -22,13 +22,15 @@ Sim.Mod = Sim.Mod.Set('Phase','stance','LinearMotor','out');
 % Init controller:                                                                    short             extend
 Sim.Con = Sim.Con.Set('Period',1.2,'phi_tau',[0.1 0.25 0.4 0.99],'tau',[2 -3],...
                        'phi_reflex',[ 0.897308852443590  0.558365283467955 ]); %0.897308852443590   0.558365283467955
+% Sim.Con = Sim.Con.Set('Period',1.2,'phi_tau',[0.1 0.25 0.4 0.99],'tau',[2 -3],...
+%                        'phi_reflex',[ NaN  NaN ]); %0.897308852443590   0.558365283467955
 Sim.Con.Controller_Type = 'CPG';
 
 
 Sim.IClimCyc = [ 0.359391210427835  -3.525464278622958  0   2.970203654740103   0.710126001714929] ;
 
 Sim.Con.IC = Sim.IClimCyc(5); % LC
-
+%Sim.Con.IC = 0.707;
     
 Sim.Con.Init();
 
@@ -37,7 +39,7 @@ Sim.Mod.IC =   Sim.IClimCyc(1:4); % LC
 
 % add disturbance:
 Sim.Mod.IC(1) =  Sim.Mod.IC(1);%-2.056; 
-
+%Sim.Mod.IC(2) = -3.505; 
 
 Sim = Sim.Init();
 
