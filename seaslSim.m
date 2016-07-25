16862f5d66b52df9c4aa1090f64d6fc691d39f70
Izzys <izzy.schall@gmail.com>
2016-07-25 13:52:43 +0300

    Merge remote-tracking branch 'origin/Simulation' into Simulation
    # Conflicts:
    #	seaslSim.m

--- seaslSim.m
+++ seaslSim.m
@@ -9,25 +9,12 @@ Sim.EndCond = 2;
 Sim = Sim.SetTime(0,0.01,100);
 
 % set terrain:
-Sim.Env.DisturbanceMaxHeight = 0.03;
+Sim.Env.DisturbanceMaxHeight = 0.00;
 Sim.Env.DisturbanceSign = 1;
 Sim.Env.start_x = 2;
 Sim.Env.end_x = 4;
 
 % Set up the model:
-
-% <<<<<<< HEAD
-% % Init controller:
-% Sim.Con = Sim.Con.Set('Period',1.2,'phi',[0.1 0.25 0.4 0.99],'tau',[2 -0.9]); 
-% Sim.Con.Controller_Type = 'CPG';%'Const';%
-% Sim.Con.IC = 0;%[1;0;Sim.Con.omega0;1;0;];
-% Sim.Con.Init();
-% 
-% % Simulate:
-% Sim.Mod.IC = [ -0.601243027678524   1.232622029427107  0   3.205736429418639]'; %for reflex at extend and short
-% %Sim.Mod.IC = [ -0.3588  -2.4  0   3.3]'; %for reflex at extend
-% =======
-
 Sim.Mod = Sim.Mod.Set('Phase','stance','LinearMotor','out');
 
 
@@ -42,12 +29,10 @@ Sim.Con.Controller_Type = 'CPG';
 Sim.IClimCyc = [ 0.359391210427835  -3.525464278622958  0   2.970203654740103   0.710126001714929] ;
 
 Sim.Con.IC = Sim.IClimCyc(5); % LC
-%Sim.Con.IC = 0.707;
     
 Sim.Con.Init();
 
 % note that if IC match the stance phase - only the first two IC count:
-
 Sim.Mod.IC =   Sim.IClimCyc(1:4); % LC
 
 % add disturbance:
